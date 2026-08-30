44a3009ca1f2916401f1769600933394f4e6f13c
Aaron <niceboy@duck.com>
2026-08-30 23:12:35 +0800

    style: hardware.nix 布局分区优化
    加文件头横幅与分区注释，行为零变化。

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -9,15 +9,12 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
   hardware'.nvidia.enable = true;
   boot'.systemd-boot.enable = true;
 
   boot = {
-    # 最新主线内核；若 580 legacy 驱动对它编译失败，注释掉这行回退默认内核
     kernelPackages = pkgs.linuxPackages_latest;
+
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
     kernelModules = ["kvm-amd"];
     kernelParams = [
@@ -47,4 +44,7 @@
     memoryPercent = 100;
     memoryMax = 16 * 1024 * 1024 * 1024 + (1024 * 1024);
   };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
