23ca38a9f8ac3da144b4e7057044838f6a346382
Aaron <niceboy@duck.com>
2026-08-30 22:23:36 +0800

    chore: 更新用户信息与 SSH 公钥

--- vars/default.nix
+++ vars/default.nix
@@ -1,14 +1,14 @@
 {
   username = "cai";
-  fullName = "Aaron";
-  email = "niceboy@duck.com";
+  fullName = "cai";
+  email = "122464806+cpdd52djb@users.noreply.github.com";
 
   timeZone = "Asia/Shanghai";
 
   hashedPassword = "$y$j9T$fXIHIyb1usprTzAw.ntqJ/$I/sbwudS.KESDGLwKV8QzsLqr7pNQvYYv20GcWKFsV1";
 
   sshAuthorizedKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6xNNhF6jaPKuch8vSHwHTGlbyn4i2zSHxrqGOiacxG Aaron@Deployment"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHjMAQUXfyMv8TG1NfqjmQJG3gqZkh25KAvAMvxVrWS Aaron@MacBook-Pro"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII6gKYOP1MdYJDMaYaO11Lcqdky7sBZDLA4OBopdYlwn"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKHjMAQUXfyMv8TG1NfqjmQJG3gqZkh25KAvAMvxVrWS"
   ];
 }
