4c9bfc71aa69aef3f7516a774c671a62cecd259e
Aaron <niceboy@duck.com>
2026-08-31 19:46:42 +0800

    feat: 移除 coding-agents 与 KDE 模块

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -16,10 +16,8 @@
 
   tools'.mise.enable = true;
   tools'.dev.enable = true;
-  tools'.coding-agents.enable = true;
   shells'.fish.enable = true;
 
-  desktop'.kde.enable = true;
   security'.firewall.enable = true;
 
   system.stateVersion = "26.05";
